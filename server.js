48349e7f9abaf47b00c0159ca85e0145bcefced6
Joao Nzango <joaonzangoii@hotmail.com>
2020-10-27 23:57:46 +0100

    Node JS server using ExpressJS

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081",
+  origin: "http://localhost:4200"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const db = require("./app/models");
+db.sequelize.sync();
+
+db.sequelize.sync({ force: false }).then(() => {
+    console.log("Drop and re-sync db.");
+});
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to Tchiowa Hacker application." });
+});
+
+require("./app/routes/tutorial.routes")(app);
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
